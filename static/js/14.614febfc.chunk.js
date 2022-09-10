fa5a9977b2da624d6b19376c0b4e4c1424e09cdd
Alabi Joshua Olufemi <femijoshuaalabi@gmail.com>
2022-09-10 14:12:07 +0100

    Updates

---
+++ static/js/14.614febfc.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonp=this.webpackJsonp||[]).push([[14],{413:function(t,a,n){"use strict";n.d(a,"a",(function(){return c}));var r=n(0);function c(t){return Object(r.a)({tag:"svg",attr:{fill:"currentColor",viewBox:"0 0 16 16"},child:[{tag:"path",attr:{d:"M4 0h5.293A1 1 0 0 1 10 .293L13.707 4a1 1 0 0 1 .293.707V14a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2zm5.5 1.5v2a1 1 0 0 0 1 1h2l-3-3z"}}]})(t)}}}]);
+//# sourceMappingURL=14.614febfc.chunk.js.map
